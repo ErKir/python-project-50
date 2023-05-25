0ebd90687e3c493443565d6d2b6d8269408a997f
ErKir <kirillerohin@gmail.com>
2023-05-25 18:02:50 +0300

    feat: tests

---
+++ test/test_gendiff.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import os
+from gendiff.scripts.build_diff import generate_diff
+
+
+def get_fixture_path(file_name):
+    return os.path.join('test/fixtures', file_name)
+
+
+result = open(get_fixture_path('result.txt'))
+expected = result.read()
+
+
+def test_with_json():
+    filepath1 = get_fixture_path('file1.json')
+    filepath2 = get_fixture_path('file2.json')
+    actual = generate_diff(filepath1, filepath2)
+    assert actual == expected
