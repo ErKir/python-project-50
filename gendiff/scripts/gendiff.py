ec77e25740ce2adb91615b74799911dbd95ab463
ErKir <kirillerohin@gmail.com>
2023-07-06 18:29:02 +0300

    fix: add print result

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -24,7 +24,7 @@ def main():
     )
 
     args = parser.parse_args()
-    build_diff(args.first_file, args.second_file, args.format)
+    print(build_diff(args.first_file, args.second_file, args.format))
 
 
 if __name__ == '__main__':
